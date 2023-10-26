9a43d1af31552b65192e943d01f04fcaa1589f32
void <valdis.urbans@gmail.com>
2023-10-26 14:50:41 +0200

    change

--- EmployeeCommonLibrary/ApiEmployee.cs
+++ EmployeeCommonLibrary/ApiEmployee.cs
@@ -1,11 +1,10 @@
-﻿using EmployeeCommonLibrary;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeApiLibrary
+namespace EmployeeCommonLibrary
 {
 
     public class ApiEmployee
